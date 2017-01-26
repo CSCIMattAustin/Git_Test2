b8cf0375c88c6532e98518ebe5b14ab0b2d4ecb5
matgaust <matgaust@CSG1-522509>
2017-01-26 12:28:59 -0600

    Added a guessing game with random numbers within a range

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,46 @@ namespace Git_test
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
             Console.WriteLine("Hello, World!!");
+            Console.WriteLine("Enter the Starting number please.");
+
+
+            string temp_first = Console.ReadLine();
+            int first_num = Convert.ToInt32(temp_first);
+
+            Console.WriteLine("Enter the ending number please.");
+
+            string temp_second = Console.ReadLine();
+            int second_num = Convert.ToInt32(temp_second);
+
+            Console.WriteLine(second_num);
+
+            if (first_num > second_num)
+            {
+                int temp;
+                temp = first_num;
+                first_num = second_num;
+                second_num = temp;
+            }
+
+            int correct_number;
+            correct_number = r.Next(first_num, second_num);
+
             Console.WriteLine("Guess a number!");
-            int number = Console.Read();
-            while(number != 42)
+
+            string first_guess = Console.ReadLine();
+            int number = Convert.ToInt32(first_guess);
+
+            while (number != correct_number)
             {
                 Console.WriteLine("Try again :(");
-                number = Console.Read();
+                first_guess = Console.ReadLine();
+                number = Convert.ToInt32(first_guess);
             }
-           
+
+            Console.WriteLine("You are correct!!");
+            Console.ReadKey();
         }
     }
 }
